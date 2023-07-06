a893f60d85aad2f29167169e99e83299a31bf92a
Ahmad-Fuad <ahmed.bashewith@gmail.com>
2023-07-06 11:51:18 +0300

    made some parameters optional

--- macros/generate_translated_table_v2.sql
+++ macros/generate_translated_table_v2.sql
@@ -1,9 +1,12 @@
 {#-- Macro to generate a translated table from the submissions and choices table pushed by Airbyte Ona Data connector and remove group names recursively with column exclusion parameter --#}
-{#-- Sample usage: in a staging model, {{ translate_table_remove_group_name('airbyte', 'sbm__table_name', 'chc__table_name', 'French (fr)', ['group1/column1', 'group2/column1']) }} --#}
+{#-- Optional parameters and their defaults: default value of labelstable = "chc__" ~ rawtable[5:], default value of language = "English (en)", exclude_column = [] --#}
+{#-- Example of minimum usage in a model, this utilizes defaults specified above: {{ translate_table_remove_group_name('airbyte', 'sbm__table_name'}} --#}
+{#-- Example of usage in a model with specified language: {{ translate_table_remove_group_name('airbyte', 'sbm__table_name', language='French (fr)' }} --#}
 
-{% macro translate_table_remove_group_name(source_name, rawtable, labelstable, language, exclude_columns=[]) %}
+{% macro translate_table_remove_group_name(source_name, rawtable, labelstable=None, language=None, exclude_columns=[]) %}
 
-
+{%- set labelstable = "chc__" ~ rawtable[5:] if labelstable is none else labelstable -%}
+{%- set language = "English (en)" if language is none else language -%}
 {%- set fieldlist =  dbt_utils.get_filtered_columns_in_relation(from=source(source_name, rawtable)) -%}  
 {%- set optionslist = dbt_utils.get_column_values(table=source(source_name, labelstable), column='field') |list %}
 
