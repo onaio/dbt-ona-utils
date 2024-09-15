9d0bc85a549b7081726ce62e20fadb4a5574290f
Alessandro Pietrobon <apietrobon@ona.io>
2024-09-15 08:56:29 +0000

    update remove_group_name

--- macros/remove_group_name.sql
+++ macros/remove_group_name.sql
@@ -3,6 +3,11 @@
 -- Sample usage with source as input: {{ remove_group_name('source', 'table', ['group1/column1', 'group2/column1']) }} --#
 -- Sample usage with repeat table as input: {{remove_group_name(sbm_table='repeat_model_name', repeat_table=True, ['group1/column1', 'group2/column1']) }} 
 
+-- Macro to remove group names from data pushed by Airbyte Ona Data connector to Destination V2
+-- This macro can be used to refrence a source or model(in case of repeat tables) in dbt to rename all columns recursively, also provides a option for column exclusion
+-- Sample usage with source as input: {{ remove_group_name('source', 'table', ['group1/column1', 'group2/column1']) }} --#
+-- Sample usage with repeat table as input: {{remove_group_name(sbm_table='repeat_model_name', repeat_table=True, ['group1/column1', 'group2/column1']) }} 
+
 {% macro remove_group_name(source_name, sbm_table, xfm_table=None, repeat_table=False, exclude_columns=[]) %}
 
     {% if not repeat_table %}
@@ -12,22 +17,20 @@
     {% else %}
         {%- set fieldlist = dbt_utils.get_filtered_columns_in_relation(from=ref(sbm_table)) -%}
     {% endif %}
-
+    
     select 
     {% for field in fieldlist %}
-        {%- if not repeat_table and field not in exclude_columns %}
-            "{{ field }}" as "{{ ona_utils.find_index(xfm_dict['airbyte_name'], field, xfm_dict['field']) }}"
-        {%- elif repeat_table and field not in exclude_columns and '/' in field %}
-            "{{ field }}" as {{ field[field.rfind('/')+1:] }}
+    {% if field not in exclude_columns %}
+        {%- if not repeat_table %}
+            "{{ field }}" as "{{ ona_utils.find_index(xfm_dict['airbyte_name'], field, xfm_dict['field']) }}",
+        {%- elif repeat_table and '/' in field %}
+            "{{ field }}" as {{ field[field.rfind('/')+1:] }},
         {%- else -%}
-            "{{ field }}"
+            "{{ field }}",
         {%- endif %}
-        {%- if not loop.last -%} 
-            ,
-        {%- else -%}
-            {{ "\n" }}
-        {%- endif -%}
+    {% endif %}
     {%- endfor -%}
+    now() as dbt_processed_at
     from 
     {%- if not repeat_table %}
         {{ source(source_name, sbm_table) }}
